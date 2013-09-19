1becfaad19f7e46a264cdd6659b9fb85d3f26277
Cory Gwin <gwincr11@yahoo.com>
2013-09-19 10:39:49 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LizBlog::Application.config.secret_key_base = '362de82616ef17e073b5fff4796f97b60b1b8e4ea02f46469bfb3ade5ac23c9b60d9db2c9e4c7bc07242c3bbd69e010147d401a8e4cbb9344bb98bc5235805f3'
